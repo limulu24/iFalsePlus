ab4e9c88306b65b5e9a046bbc27baa1d411ae9c0
limulu <limulu24@outlook.com>
2025-01-20 20:31:19 +0800

    limulu1.7.5

--- searchform-mobile.php
+++ searchform-mobile.php
@@ -1,6 +1,6 @@
 <form role="searchform-mb" method="get" id="searchform-mb" class="searchform-mb" action="<?php bloginfo('url') ?>">
 	<div>
-		<input type="text-" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="开始搜索吧杂鱼~" required>
+		<input type="text-" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="开始搜索吧~" required>
         <button type="submit"><span class="iconfont icon-sousuo"></span></button>
 	</div>
 </form>
